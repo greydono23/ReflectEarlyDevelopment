c3d4a25b8ec0abc07e8cb4308f1f0ec62b58bed7
greydono23 <greywok@icloud.com>
2023-02-25 15:05:42 -0500

    Initial Commit

---
+++ ReflectEarlyTesting2/ReflectEarlyTesting2App.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectEarlyTesting2App.swift
+//  ReflectEarlyTesting2
+//
+//  Created by Greydon O'Keefe on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ReflectEarlyTesting2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
